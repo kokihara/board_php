3f9c0ec2e6c59dc040f3098355d8668b2eeb468d
kokihara <nov17hk@gmail.com>
2021-12-30 23:33:48 +0900

    お問い合わせ実装

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    var_dump($_POST);
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -273,10 +276,22 @@ article.reply::before {
     }
 }
 </style>
+
 </head>
 <body>
 <h1>ひと言掲示板</h1>
 <!-- ここにメッセージの入力フォームを設置 -->
+<form method="post">
+    <div>
+        <label for="view_name">表示名</label>
+        <input id="view_name" type="text" name="view_name" value="">
+    </div>
+    <div>
+        <label for="message">ひと言メッセージ</label>
+        <textarea name="message" id="message"></textarea>
+    </div>
+        <input type="submit" name="btn_submit" value="書き込む">
+</form>
 <hr>
 <section>
 <!-- ここに投稿されたメッセージを表示 -->
